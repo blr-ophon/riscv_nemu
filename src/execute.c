8564394801ae7ef161ed73cb22c1ca0189b9ad48
blrophon <gabriel.pequod@gmail.com>
2025-03-28 02:56:31 -0300

    Store and Branch

--- src/execute.c
+++ src/execute.c
@@ -123,4 +123,86 @@ int ExecuteI_Aluopi(RVCore *core, ParsedInstruction *p_inst){
 }
 
 int ExecuteI_Load(RVCore *core, ParsedInstruction *p_inst){
+    int rv = 0;
+    uint32_t x0_dummy;
+    uint32_t *rd = p_inst->rd == 0? &x0_dummy : &core->gpr[p_inst->rd];
+    uint32_t rs1 = p_inst->rd == 0? 0 : core->gpr[p_inst->rs1];
+    uint32_t imm = p_inst->imm == 0? 0 : p_inst->imm;
+
+    switch(p_inst->func3){
+        case 0x0:   // LB
+            *rd = (core->memory[rs1 + imm]) & 0xff;
+            if(*rd & (1 << 7)){
+                *rd |= 0xffffff;
+            }
+            break;
+        case 0x1:   // LH
+            *rd = (core->memory[rs1 + imm]) & 0xffff;
+            if(*rd & (1 << 15)){
+                *rd |= 0xffff;
+            }
+            break;
+        case 0x2:   // LW
+            *rd = core->memory[rs1 + imm];
+            break;
+        case 0x4:   // LBU
+            *rd = (core->memory[rs1 + imm]) & 0xff;
+            break;
+        case 0x5:   // LHU
+            *rd = (core->memory[rs1 + imm]) & 0xffff;
+            break;
+    }
+
+    return rv;
+}
+
+
+int Execute_Store(RVCore *core, ParsedInstruction *p_inst){
+    int rv = 0;
+    uint32_t rs1 = p_inst->rd == 0? 0 : core->gpr[p_inst->rs1];
+    uint32_t rs2 = p_inst->rd == 0? 0 : core->gpr[p_inst->rs2];
+    uint32_t imm = p_inst->imm == 0? 0 : p_inst->imm;
+
+    switch(p_inst->func3){
+        case 0x0:   // SB
+            core->memory[rs1+imm] = rs2 & 0xff;
+            break;
+        case 0x1:   // SH
+            core->memory[rs1+imm] = rs2 & 0xffff;
+            break;
+        case 0x2:   // SW
+            core->memory[rs1+imm] = rs2;
+            break;
+    }
+
+    return rv;
+}
+
+int Execute_Branch(RVCore *core, ParsedInstruction *p_inst){
+    int rv = 0;
+    uint32_t rs1 = p_inst->rd == 0? 0 : core->gpr[p_inst->rs1];
+    uint32_t rs2 = p_inst->rd == 0? 0 : core->gpr[p_inst->rs2];
+    uint32_t imm = p_inst->imm == 0? 0 : p_inst->imm;
+
+    switch(p_inst->func3){
+        case 0x0:   // BEQ
+            if(rs1 == rs2) core->pc += imm;
+            break;
+        case 0x1:   // BNE
+            if(rs1 != rs2) core->pc += imm;
+            break;
+        case 0x4:   // BLT
+            if((int32_t) rs1 < (int32_t) rs2) core->pc += imm;
+            break;
+        case 0x5:   // BGE
+            if((int32_t) rs1 >= (int32_t) rs2) core->pc += imm;
+            break;
+        case 0x6:   // BLTU
+            if(rs1 < rs2) core->pc += imm;
+            break;
+        case 0x7:   // BGEU
+            if(rs1 >= rs2) core->pc += imm;
+            break;
+    }
+    return rv;
 }
